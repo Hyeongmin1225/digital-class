1cd72ec14e5dd9b399be9d1fb62f1b9309452a4f
Hyeoungmi1225 <p991225@blah.com>
2023-04-05 17:32:18 +0900

    2023/04/05 .17:32

--- programmers/python3/120585.py
+++ programmers/python3/120585.py
@@ -1,11 +1,8 @@
 #1번째줄 array, height 라는 변수를 설정하여
 #2번째줄 결과 answer을 0으로 초기값을 주고
 #3번째줄 for문에서 
-
-
-
-
-
+# park hyeong min eun baboda.
+# jabini parke morunun babo.kkk
 
 def solution(array, height):
     answer = 0
--- programmers/python3/120808.py
+++ programmers/python3/120808.py
@@ -1,6 +1,9 @@
+from fractions import Fraction
 def solution(numer1, denom1, numer2, denom2):
-    answer = []
-    answer = numer1/denom1 + numer2/denom2
+    fraction1 = Fraction(numer1,denom1)
+    fraction2 = Fraction(numer2,denom2)
+    answer =fraction1 + fraction2
+    answer = [answer.numerator,answer.denominator ]
     return answer
 
 print(solution (1,2,3,4))
